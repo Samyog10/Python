57c2c3f1f45ba6ce77949e1a7d5512ddcee30e78
unknown <paudelsamyog2@gmail.com>
2024-12-04 09:04:45 +0545

    change

---
+++ main.py
@@ -0,0 +1,24 @@
+
+import logging
+
+from mathematics.trigonometry import cosine, sine
+
+
+# Configure logging (log to console by default)
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
+# different log levels
+logging.debug("This is a debug message")
+logging.info("This is an info message")
+logging.warning("This is a warning message")
+logging.error("This is an error message")
+logging.critical("This is a critical message")
+
+
+try:
+    100 / 0  
+except ZeroDivisionError as e:  
+    logging.error(f"Error: {e}")
+
+
+
